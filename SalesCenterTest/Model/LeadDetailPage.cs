4d2c94068630948721a64323037d8e2fe75d3586
Ningappa-workwave <ntambad_evry@workwave.com>
2021-10-26 15:31:12 +0530

    WWM_LD_006 fixes

--- SalesCenterTest/Model/LeadDetailPage.cs
+++ SalesCenterTest/Model/LeadDetailPage.cs
@@ -218,7 +218,7 @@ namespace WorkWave.PestPac.TA.Model
         [FindsBy(How = How.XPath, Using = "//span[text()='Apply']/..")]
         private IWebElement ClickApplyButton { get { return PageFactory.Load(this); } }
 
-        [FindsBy(How = How.XPath, Using = "//div[@class='sc-iklJeh hbusih actions']/descendant::button[2]")]
+        [FindsBy(How = How.XPath, Using = "//div[@class='sc-idOiZg gPAFHC actions']/descendant::button[2]")]
         private IWebElement ClickThreeDotsIconforDisqualified { get { return PageFactory.Load(this); } }
 
         [FindsBy(How = How.XPath, Using = "//span[text()='Re-Open']")]
